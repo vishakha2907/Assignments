d40b5e92eb1ea2d90c9fbf919d7687d2017b4aef
vishakha2907 <78352835+vishakha2907@users.noreply.github.com>
2021-02-14 12:54:27 +0530

    Create count.py

---
+++ Digit/count.py
@@ -0,0 +1,11 @@
+def countDigit(n):
+    count = 0
+    while n != 0:
+        n //= 10
+        count += 1
+    return count
+ 
+ 
+# Driver Code
+n = 345289467
+print("Number of digits : % d" % (countDigit(n)))
